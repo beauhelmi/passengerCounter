b8510791e283ad02c0b61e2cac8208763ad22b3f
b34u <beauhelmi@gmail.com>
2022-10-07 21:45:52 +0800

    done with btn increment and grab count-el element and store it in countEl variable

---
+++ index.js
@@ -0,0 +1,18 @@
+
+/* instructions for function increment*/
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the new count
+
+
+// grab the count-el element, store it in a countEl variable
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    // set countEl's innerText to the count
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+//Passenger counter using Javascript
+//Tutorials from scrimba free Javascript course
+
+/* instructions for function increment*/
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the new count
